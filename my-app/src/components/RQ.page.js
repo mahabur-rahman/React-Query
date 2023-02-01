72899914fed5b2bcec1d5c02c97922996c32d69d
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2023-02-01 16:23:50 +0600

    data fetch using react query

--- my-app/src/components/RQ.page.js
+++ my-app/src/components/RQ.page.js
@@ -1,7 +1,33 @@
+import axios from "axios";
 import React from "react";
+import { useQuery } from "react-query";
+
+const fetchSuperHeroes = () => {
+  return axios.get(`http://localhost:4000/superheroes`);
+};
 
 const RQ = () => {
-  return <div>RQ page</div>;
+  //   const { isLoading, data } = useQuery("super-heroes", () => {
+  //     return axios.get(`http://localhost:4000/superheroes`);
+  //   });
+
+  const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
+
+  if (isLoading) {
+    return <h1>Loading..</h1>;
+  }
+
+  console.log(data);
+
+  return (
+    <>
+      <h2>Rq super heroes data fetch</h2>
+
+      {data?.data.map((ele) => {
+        return <div key={ele.name}>{ele.name}</div>;
+      })}
+    </>
+  );
 };
 
 export default RQ;
